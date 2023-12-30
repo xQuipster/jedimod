dc71fba41e53dd8f87502c46161e140889ce1d98
xQuipster <152291100+xQuipster@users.noreply.github.com>
2023-12-30 11:24:01 +0200

    update

--- src/main/java/com/xquipster/jedimod/JediMod.java
+++ src/main/java/com/xquipster/jedimod/JediMod.java
@@ -3,9 +3,6 @@ package com.xquipster.jedimod;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
@@ -14,18 +11,14 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.relauncher.Side;
 import org.lwjgl.input.Keyboard;
 
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 @Mod(modid = JediMod.MOD_ID, name = JediMod.NAME, version = JediMod.VERSION)
 public class JediMod
@@ -50,8 +43,6 @@ public class JediMod
     public ArrayList<KeyBinding> keyBindings;
     public ArrayList<String> ips;
     public ArrayList<String> ips1;
-
-    public HashMap<ItemStack, Integer> timerAbilities = new HashMap<>();
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
@@ -88,7 +79,7 @@ public class JediMod
             InputStream is = con.getInputStream();
 
             try(BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-                String line = null;
+                String line;
 
                 // read each line and write to System.out
                 while ((line = br.readLine()) != null) {
@@ -105,7 +96,7 @@ public class JediMod
             InputStream is = con.getInputStream();
 
             try(BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
-                String line = null;
+                String line;
 
                 // read each line and write to System.out
                 while ((line = br.readLine()) != null) {
@@ -115,13 +106,11 @@ public class JediMod
         }catch (Exception ignored){
         }
         MinecraftForge.EVENT_BUS.register(this);
-        NetworkRegistry.INSTANCE.newSimpleChannel("jedimod").registerMessage(JediMessage.MessageHandler.class, JediMessage.class, 0, Side.SERVER);
 
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
         ClientRegistry.registerKeyBinding(lightningBind);
         ClientRegistry.registerKeyBinding(pushBind);
         ClientRegistry.registerKeyBinding(jumpBind);
@@ -133,43 +122,6 @@ public class JediMod
         ClientRegistry.registerKeyBinding(shieldBind);
         ClientRegistry.registerKeyBinding(reflectionBind);
         ClientRegistry.registerKeyBinding(blockerBind);
-    }
-    @SubscribeEvent
-    public void onJoinServer(RenderGameOverlayEvent event){
-        if (event.getType() == RenderGameOverlayEvent.ElementType.ALL && Minecraft.getMinecraft().getCurrentServerData() != null){
-            boolean a = false;
-            for (String ip : ips){
-                if (ip.equalsIgnoreCase(Minecraft.getMinecraft().getCurrentServerData().serverIP)){
-                    a = true;
-                    break;
-                }
-            }
-            boolean b = false;
-            for (String ip : ips1){
-                if (ip.equalsIgnoreCase(Minecraft.getMinecraft().getCurrentServerData().serverIP)){
-                    b = true;
-                    break;
-                }
-            }
-            if (a || b){
-                HashMap<ItemStack, Integer> abilities = getTimerAbilites();
-                if (!abilities.isEmpty()){
-                    int y = 8;
-                    for (ItemStack stack : abilities.keySet()){
-                        assert Minecraft.getMinecraft().currentScreen != null;
-                        Minecraft.getMinecraft().getRenderItem().renderItemIntoGUI(stack, Minecraft.getMinecraft().currentScreen.width - 64, y);
-                        Minecraft.getMinecraft().fontRenderer.drawStringWithShadow("" + abilities.get(stack), Minecraft.getMinecraft().currentScreen.width - 32, y, Color.white.getRGB());
-                        y+=16;
-                    }
-                }
-            }
-        }
-    }
-
-    private HashMap<ItemStack, Integer> getTimerAbilites() {
-        String msg = "/jfsjjdkfj";
-        Minecraft.getMinecraft().player.sendChatMessage(msg);
-        return timerAbilities;
     }
 
     @SubscribeEvent

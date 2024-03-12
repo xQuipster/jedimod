8f00eba07f0a7d213f1280263e04ace5c714e4bc
xQuipster <152291100+xQuipster@users.noreply.github.com>
2024-03-12 14:25:45 +0200

    1

---
+++ src/main/java/com/xquipster/jedimod/api/PlayerModel.java
@@ -0,0 +1,185 @@
+package com.xquipster.jedimod.api;
+
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.EnumHandSide;
+
+public class PlayerModel extends ModelBiped
+{
+    public ModelRenderer bipedLeftArmwear;
+    public ModelRenderer bipedRightArmwear;
+    public ModelRenderer bipedLeftLegwear;
+    public ModelRenderer bipedRightLegwear;
+    public ModelRenderer bipedBodyWear;
+    private final ModelRenderer bipedCape;
+    private final boolean smallArms;
+
+    public PlayerModel(float modelSize, boolean smallArmsIn, boolean hdSkin, boolean hdCape)
+    {
+        super(modelSize, 0.0F, 64 * (hdSkin ? 16 : 1), 64 * (hdSkin ? 16 : 1));
+        int i = hdSkin ? 16 : 1;
+        int i1 = hdCape ? 2 : 1;
+        this.smallArms = smallArmsIn;
+        this.bipedCape = new ModelRenderer(this, 0, 0);
+        bipedCape.textureHeight/=i1;
+        this.bipedCape.setTextureSize(64, 32);
+        this.bipedCape.addBox(-5.0F, 0.0F, -1.0F, 10, 16, 1, modelSize);
+        bipedHead.textureHeight*=i;
+        bipedHead.textureWidth*=i;
+        bipedHeadwear.textureWidth*=i;
+        bipedHeadwear.textureHeight*=i;
+        bipedHeadwear.setTextureOffset(0, 32 * i);
+        bipedBody.textureWidth*=i;
+        bipedBody.textureHeight*=i;
+        bipedBody.setTextureOffset(16 * i, 16 * i);
+
+        if (smallArmsIn)
+        {
+            this.bipedLeftArm = new ModelRenderer(this, 32 * i, 48 * i);
+            bipedLeftArm.textureHeight*=i;
+            bipedLeftArm.textureWidth*=i;
+            this.bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 3, 12, 4, modelSize);
+            this.bipedLeftArm.setRotationPoint(5.0F, 2.5F, 0.0F);
+            this.bipedRightArm = new ModelRenderer(this, 40 * i, 16 * i);
+            bipedRightArm.textureWidth*=i;
+            bipedRightArm.textureHeight*=i;
+            this.bipedRightArm.addBox(-2.0F, -2.0F, -2.0F, 3, 12, 4, modelSize);
+            this.bipedRightArm.setRotationPoint(-5.0F, 2.5F, 0.0F);
+            this.bipedLeftArmwear = new ModelRenderer(this, 48 * i, 48 * i);
+            bipedLeftArmwear.textureWidth*=i;
+            bipedLeftArmwear.textureHeight*=i;
+            this.bipedLeftArmwear.addBox(-1.0F, -2.0F, -2.0F, 3, 12, 4, modelSize + 0.25F);
+            this.bipedLeftArmwear.setRotationPoint(5.0F, 2.5F, 0.0F);
+            this.bipedRightArmwear = new ModelRenderer(this, 40 * i, 32 * i);
+            bipedRightArmwear.textureWidth*=i;
+            bipedRightArmwear.textureHeight*=i;
+            this.bipedRightArmwear.addBox(-2.0F, -2.0F, -2.0F, 3, 12, 4, modelSize + 0.25F);
+            this.bipedRightArmwear.setRotationPoint(-5.0F, 2.5F, 10.0F);
+        }
+        else
+        {
+            this.bipedLeftArm = new ModelRenderer(this, 32 * i, 48 * i);
+            bipedLeftArm.textureHeight*=i;
+            bipedLeftArm.textureWidth*=i;
+            this.bipedLeftArm.addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4, modelSize);
+            this.bipedLeftArm.setRotationPoint(5.0F, 2.0F, 0.0F);
+            this.bipedLeftArmwear = new ModelRenderer(this, 48 * i, 48 * i);
+            bipedLeftArmwear.textureWidth*=i;
+            bipedLeftArmwear.textureHeight*=i;
+            this.bipedLeftArmwear.addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4, modelSize + 0.25F);
+            this.bipedLeftArmwear.setRotationPoint(5.0F, 2.0F, 0.0F);
+            this.bipedRightArmwear = new ModelRenderer(this, 40 * i, 32 * i);
+            bipedRightArmwear.textureWidth*=i;
+            bipedRightArmwear.textureHeight*=i;
+            this.bipedRightArmwear.addBox(-3.0F, -2.0F, -2.0F, 4, 12, 4, modelSize + 0.25F);
+            this.bipedRightArmwear.setRotationPoint(-5.0F, 2.0F, 10.0F);
+        }
+        this.bipedLeftLeg = new ModelRenderer(this, 16 * i, 48 * i);
+        bipedLeftLeg.textureWidth*=i;
+        bipedLeftLeg.textureHeight*=i;
+        this.bipedLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, modelSize);
+        this.bipedLeftLeg.setRotationPoint(1.9F, 12.0F, 0.0F);
+        this.bipedLeftLegwear = new ModelRenderer(this, 0, 48 * i);
+        bipedLeftLegwear.textureWidth*=i;
+        bipedLeftLegwear.textureHeight*=i;
+        this.bipedLeftLegwear.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, modelSize + 0.25F);
+        this.bipedLeftLegwear.setRotationPoint(1.9F, 12.0F, 0.0F);
+        this.bipedRightLegwear = new ModelRenderer(this, 0, 32 * i);
+        bipedRightLegwear.textureHeight*=i;
+        bipedRightLegwear.textureWidth*=i;
+        this.bipedRightLegwear.addBox(-2.0F, 0.0F, -2.0F, 4, 12, 4, modelSize + 0.25F);
+        this.bipedRightLegwear.setRotationPoint(-1.9F, 12.0F, 0.0F);
+        this.bipedBodyWear = new ModelRenderer(this, 16 * i, 32 * i);
+        bipedBodyWear.textureWidth*=i;
+        bipedBodyWear.textureHeight*=i;
+        this.bipedBodyWear.addBox(-4.0F, 0.0F, -2.0F, 8, 12, 4, modelSize + 0.25F);
+        this.bipedBodyWear.setRotationPoint(0.0F, 0.0F, 0.0F);
+    }
+
+    public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale)
+    {
+        super.render(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+        GlStateManager.pushMatrix();
+
+        if (this.isChild)
+        {
+            float f = 2.0F;
+            GlStateManager.scale(0.5F, 0.5F, 0.5F);
+            GlStateManager.translate(0.0F, 24.0F * scale, 0.0F);
+            this.bipedLeftLegwear.render(scale);
+            this.bipedRightLegwear.render(scale);
+            this.bipedLeftArmwear.render(scale);
+            this.bipedRightArmwear.render(scale);
+            this.bipedBodyWear.render(scale);
+        }
+        else
+        {
+            if (entityIn.isSneaking())
+            {
+                GlStateManager.translate(0.0F, 0.2F, 0.0F);
+            }
+
+            this.bipedLeftLegwear.render(scale);
+            this.bipedRightLegwear.render(scale);
+            this.bipedLeftArmwear.render(scale);
+            this.bipedRightArmwear.render(scale);
+            this.bipedBodyWear.render(scale);
+        }
+
+        GlStateManager.popMatrix();
+    }
+
+    public void renderCape(float scale)
+    {
+        this.bipedCape.render(scale);
+    }
+
+    public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn)
+    {
+        super.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scaleFactor, entityIn);
+        copyModelAngles(this.bipedLeftLeg, this.bipedLeftLegwear);
+        copyModelAngles(this.bipedRightLeg, this.bipedRightLegwear);
+        copyModelAngles(this.bipedLeftArm, this.bipedLeftArmwear);
+        copyModelAngles(this.bipedRightArm, this.bipedRightArmwear);
+        copyModelAngles(this.bipedBody, this.bipedBodyWear);
+
+        if (entityIn.isSneaking())
+        {
+            this.bipedCape.rotationPointY = 2.0F;
+        }
+        else
+        {
+            this.bipedCape.rotationPointY = 0.0F;
+        }
+    }
+
+    public void setVisible(boolean visible)
+    {
+        super.setVisible(visible);
+        this.bipedLeftArmwear.showModel = visible;
+        this.bipedRightArmwear.showModel = visible;
+        this.bipedLeftLegwear.showModel = visible;
+        this.bipedRightLegwear.showModel = visible;
+        this.bipedBodyWear.showModel = visible;
+        this.bipedCape.showModel = visible;
+    }
+
+    public void postRenderArm(float scale, EnumHandSide side)
+    {
+        ModelRenderer modelrenderer = this.getArmForSide(side);
+
+        if (this.smallArms)
+        {
+            float f = 0.5F * (float)(side == EnumHandSide.RIGHT ? 1 : -1);
+            modelrenderer.rotationPointX += f;
+            modelrenderer.postRender(scale);
+            modelrenderer.rotationPointX -= f;
+        }
+        else
+        {
+            modelrenderer.postRender(scale);
+        }
+    }
+}
